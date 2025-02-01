9fd6ef371e2f7ca97e98b908bab817762673422b
Elizabeth O. Edwards <103456558+qween-beth@users.noreply.github.com>
2025-02-01 22:42:58 +0000

    updated files

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ class DevelopmentConfig:
     """Development configuration."""
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///your_database_name.db'  # Example for SQLite
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///fashion_tryon.db'  # Example for SQLite
 
 class ProductionConfig:
     """Production configuration."""
     DEBUG = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///your_production_database.db'  # Example for Production Database
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///fashion_tryon.db'  # Example for Production Database
